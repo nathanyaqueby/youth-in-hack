86291e659853edcb7409abf4ee12aab19b7f8734
queby <nathanyaqueby21@gmail.com>
2023-06-21 01:27:45 +0200

    upload files

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image
+from fastai.vision.all import *
+
+
+st.title("Hello World")
+
+# load the fastai model
+learn = load_learner('export.pkl')
+
+# create a file uploader
+uploader = st.file_uploader("Upload an image...", type=["jpg","jpeg","png"])
+
+# create a button
+if st.button("Predict"):
+    # read the image
+    img = Image.create(uploader)
+    # make a prediction
+    pred, pred_idx, probs = learn.predict(img)
+    # display the prediction
+    st.write(f"Prediction: {pred}; Probability: {probs[pred_idx]:.04f}")
+    # display the image
+    st.image(img.to_thumb(500,500), caption='Uploaded Image')
